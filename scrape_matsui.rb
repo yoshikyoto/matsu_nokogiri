0c33b4b69f50cc3d2744df716c9827ea95db060e
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-02-26 01:03:31 +0900

    scrape

--- scrape_matsui.rb
+++ scrape_matsui.rb
@@ -1,12 +1,13 @@
-require 'open-uri'
+require 'yaml'
 require 'nokogiri'
+require './infra/matsu-client.rb'
 
-url = 'https://www.deal.matsui.co.jp/ITS/login/MemberLogin.jsp'
-charset = nil
-html = open(url) do |f|
-  charset = f.charset
-  f.read
-end
+config = YAML.load_file('conf/config.yml')
 
-doc = Nokogiri::HTML.parse(html, nil, charset)
+matsu = MatsuClient.new(config[:matsui][:id], config[:matsui][:pass])
+nokogiriDoc = matsu.test
+puts nokogiriDoc.title
+
+response = matsu.login
+doc = Nokogiri::HTML.parse(response.body)
 puts doc.title
